c3580f10df062f841ab55a1ca9113044129ac151
Nadja Summerscales-Gunn <nadja.summerscales@graze.com>
2022-05-19 22:17:43 +0100

    (feat) start building out messaging for how many people we can acommodate and how many people are left for seats

--- main.go
+++ main.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our party booking system.")
-	fmt.Println("For more information, please contact us at enquiries@crookhamstreet.co.uk")
+	locationName := "Crookham Street Social"
+	const maxPeople = 80
+	remainingPeople := 80
+
+	fmt.Println("Welcome to the", locationName, "table booking system.")
+	fmt.Printf("We can currently only accommodate %d people for each event. This event currently has %d seats left.\n",
+		maxPeople,
+		remainingPeople)
+	fmt.Println("For more information, please contact us at enquiries@crookhamstreet.co.uk.")
 }
