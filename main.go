cff6ea5105f43fe401f2d640ee7208a79d4069c3
Nadja Summerscales-Gunn <nadja.summerscales@graze.com>
2022-05-19 22:05:27 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to our party booking system.")
+	fmt.Println("For more information, please contact us at enquiries@crookhamstreet.co.uk")
+}
