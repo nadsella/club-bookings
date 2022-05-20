7e2bbd2901f3b165cc9042c56e546cd67578bbcb
Nadja Summerscales-Gunn <nadja.summerscales@graze.com>
2022-05-20 16:31:32 +0100

    (feat) beging to build out work to add bookings via command line prompts

--- main.go
+++ main.go
@@ -2,14 +2,59 @@ package main
 
 import "fmt"
 
-func main() {
-	locationName := "Crookham Street Social"
-	const maxPeople = 80
-	remainingPeople := 80
+const (
+	locationName = "Crookham Street Social"
+	emailAddress = "enquiries@crookhamstreet.co.uk"
+	maxPeople    = 80
+)
 
-	fmt.Println("Welcome to the", locationName, "table booking system.")
+var bookings []string
+
+func main() {
+	var remainingPeople uint = 80
+
+	fmt.Printf("Welcome to the %s table booking system.\n", locationName)
 	fmt.Printf("We can currently only accommodate %d people for each event. This event currently has %d seats left.\n",
 		maxPeople,
 		remainingPeople)
-	fmt.Println("For more information, please contact us at enquiries@crookhamstreet.co.uk.")
+	fmt.Printf("For more information on how to book, please contact us at %s.\n", emailAddress)
+
+	numPeople := bookingInformation()
+
+	remainingPeople = remainingPeople - numPeople
+
+	fmt.Printf("We now only have room for %d more people.\n", remainingPeople)
+}
+
+func bookingInformation() uint {
+	var firstName string
+	var lastName string
+	var userEmailAddress string
+	var numPeople uint
+
+	//ask the user for their name
+	fmt.Println("Please enter your first name:")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Please enter your last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Please enter your email address:")
+	fmt.Scan(&userEmailAddress)
+
+	fmt.Println("Please enter the amount of people you need a table for:")
+	fmt.Scan(&numPeople)
+
+	// add the user to the bookings array
+	bookings = append(bookings, fmt.Sprintf("%s %s", firstName, lastName))
+
+	fmt.Printf(
+		"Thank you %s %s for your booking at %s for %d people. You will receive a confirmation email at %s.\n",
+		firstName,
+		lastName,
+		locationName,
+		numPeople,
+		userEmailAddress)
+
+	return numPeople
 }
