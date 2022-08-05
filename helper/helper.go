803dac9fcd7102239ee7d01ebc637153a69d5727
Nadja Summerscales-Gunn <nadja.summerscales@graze.com>
2022-08-05 13:52:22 +0100

    (feat) create new helper package and export the validateUserData function for use in main.go

---
+++ helper/helper.go
@@ -0,0 +1,32 @@
+package helper
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	maxPeople  uint = 80
+	minNameLen int  = 2
+)
+
+func ValidateUserData(userFirstName string, userLastName string, userEmail string) bool {
+	isValidName := len(userFirstName) >= minNameLen && len(userLastName) >= minNameLen
+	isValidEmail := strings.Contains(userEmail, "@")
+
+	// if it's fine, then just return true
+	if isValidName && isValidEmail {
+		return true
+	}
+
+	if !isValidName {
+		fmt.Println("We can't reserve these spaces as your first or last name is not long enough.")
+	}
+
+	if !isValidEmail {
+		fmt.Println("We can't reserve these spaces as your email address doesn't contain an @.")
+	}
+
+	// and return false
+	return false
+}
