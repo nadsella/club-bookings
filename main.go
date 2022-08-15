dc0759b0de1ecf6bad37bf781747fb4f3ba8a123
Nadja Summerscales-Gunn <nadja.summerscales@graze.com>
2022-08-15 11:20:28 +0100

    (feat) move getNames out of main.go and reference it within the new booking package

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 		fmt.Printf("We only have room for %d more people\n", remainingPeople)
 	}
 
-	names := getNames()
+	names := booking.GetNames()
 	fmt.Printf("These are all of our bookings: %v.\n", names)
 }
 
@@ -38,20 +38,4 @@ func greetUsers() {
 		helper.MaxPeople,
 		remainingPeople)
 	fmt.Printf("For more information on how to book, please contact us at %s.\n", emailAddress)
-}
-
-// loops through the bookings and just grabs the names of the bookings
-func getNames() []string {
-	names := []string{}
-
-	for _, booking := range booking.Bookings {
-		names = append(
-			names, 
-			fmt.Sprintf(
-				"%s %s",
-				booking["firstName"],
-				booking["lastName"]))
-	}
-
-	return names
 }
