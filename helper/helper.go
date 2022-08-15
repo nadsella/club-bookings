ff9035a9c0090f4c00073bf1dfea649fb149ea18
Nadja Summerscales-Gunn <nadja.summerscales@graze.com>
2022-08-15 11:14:51 +0100

    (feat) move atCapacity function into the helper package

--- helper/helper.go
+++ helper/helper.go
@@ -7,7 +7,10 @@ import (
 
 const (
 	minNameLen int  = 2
+	MaxPeople  uint = 80
 )
+
+var RemainingPeople uint = MaxPeople
 
 func ValidateUserData(userFirstName string, userLastName string, userEmail string) bool {
 	isValidName := len(userFirstName) >= minNameLen && len(userLastName) >= minNameLen
@@ -27,5 +30,24 @@ func ValidateUserData(userFirstName string, userLastName string, userEmail strin
 	}
 
 	// and return false
+	return false
+}
+
+// checks if we're close to capacity, and if we're at capacity will let the user know
+// and returns a boolean
+func AtCapacity() bool {
+	// let people know we are close to capacity
+	// this should go out as an email to notify the bookings team
+	if RemainingPeople == 0 {
+		fmt.Println("We are at capacity!")
+
+		// we return true when were at capacity
+		return true
+	}
+
+	if RemainingPeople < 5 {
+		fmt.Println("We are nearly at capacity!")
+	}
+
 	return false
 }
